7564125eea0c8162d7832db785b7215441146981
Nick Alzapiedi <nalzapiedi@instructure.com>
2018-04-02 16:15:16 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const CANVAS_WIDTH = 1200;
+const CANVAS_HEIGHT = 900;
+
+const canvas = document.getElementById('canvas');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+const ctx = canvas.getContext('2d');
+
+const circles = [];
+const intervals = [];
+
+function clear() {
+  ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
+}
+
+function drawCircle(circle) {
+  const { x, y } = circle;
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function addCircle(circle) {
+  circle.x = CANVAS_WIDTH / 2;
+  circle.y = CANVAS_HEIGHT / 2 + circle.r;
+  circles.push(circle);
+}
+
+function stop() {
+  intervals.forEach(clearInterval);
+}
+
+function start () {
+  if (circles.length === 0) [{ r: 400, a: 0, v: Math.PI / 260 }, { r: 225, a: 0, v: Math.PI / 100 }].forEach(addCircle);
+  intervals.push(setInterval(() => {
+    circles.forEach(circle => {
+      circle.a = circle.a + circle.v === Math.PI * 2 ? 0 : circle.a + circle.v;
+      circle.x = circle.r * Math.cos(circle.a) + CANVAS_WIDTH / 2;
+      circle.y = circle.r * Math.sin(circle.a) + CANVAS_HEIGHT / 2;
+    });
+
+  }, 1000/60));
+
+  intervals.push(setInterval(() => {
+    ctx.beginPath();
+    ctx.moveTo(circles[0].x, circles[0].y);
+    ctx.lineTo(circles[1].x, circles[1].y);
+    ctx.stroke();
+  }, 30));
+}
+
+start();
