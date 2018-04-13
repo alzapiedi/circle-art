f263c35399c19d7a3595c6b313d65ae73a04afb8
Nick Alzapiedi <nalzapiedi@instructure.com>
2018-04-12 21:25:15 -0400

    wtf

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ctx = document.getElementById('canvas').getContext('2d');
   ctx.canvas.height = CANVAS_SIZE;
   ctx.canvas.width = CANVAS_SIZE;
+  ctx.canvas.style.height = `${CANVAS_SIZE}px`;
+  ctx.canvas.style.width = `${CANVAS_SIZE}px`;
   ReactDOM.render(<App ctx={ctx} />, document.getElementById('root'));
 });
