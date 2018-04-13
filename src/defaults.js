5caa5c4d288a1845204549f7bb337a7a42dcfb20
Nick Alzapiedi <nalzapiedi@instructure.com>
2018-04-13 15:14:47 -0400

    updated defaults

--- src/defaults.js
+++ src/defaults.js
@@ -1,8 +1,8 @@
 export const CANVAS_SIZE = 2000;
 export const R_1 = 850;
 export const R_2 = 600;
-export const STEPS_1 = 130;
-export const STEPS_2 = 25;
+export const STEPS_1 = 182;
+export const STEPS_2 = 112;
 
 export default {
   CANVAS_SIZE,
